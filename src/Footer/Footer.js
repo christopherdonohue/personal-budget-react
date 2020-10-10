611cd7da445c0736de8b6cdc6c62a7dd71464bc7
Chris Donohue <cdonohu4@uncc.edu>
2020-10-10 19:16:49 -0400

    Brought PB code in

---
+++ src/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Footer() {
+  return (
+    <footer className="bottom">
+        <div className="center">
+            <strong>All rights reserved &copy; Fabio Nolasco</strong>
+        </div>
+    </footer>
+  );
+}
+
+export default Footer;
