969aa949e42bc8c7520e2b89b7bcf320efb1d302
Chris Donohue <cdonohu4@uncc.edu>
2020-10-15 17:45:39 -0400

    States Implemented

--- src/App.js
+++ src/App.js
@@ -1,43 +1,35 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Menu from './Menu/Menu';
-import Hero from './Hero/Hero';
-import HomePage from './HomePage/HomePage';
-import Footer from './Footer/Footer';
-import AboutPage from './AboutPage/AboutPage';
-import LoginPage from './LoginPage/LoginPage';
-
-
+import Menu from "./Menu/Menu";
+import Hero from "./Hero/Hero";
+import HomePage from "./HomePage/HomePage";
+import Footer from "./Footer/Footer";
+import AboutPage from "./AboutPage/AboutPage";
+import LoginPage from "./LoginPage/LoginPage";
 
 function App() {
-
   return (
     <Router>
-      <Menu/>
-      <Hero/>
+      <Menu />
+      <Hero />
       <div className="mainContainer">
         <Switch>
           <Route path="/about">
-            <AboutPage/>
+            <AboutPage />
           </Route>
           <Route path="/login">
-            <LoginPage/>
+            <LoginPage />
           </Route>
           <Route path="/">
-            <HomePage/>
+            <HomePage />
           </Route>
         </Switch>
       </div>
-      <Footer/>
+      <Footer />
     </Router>
   );
 }
-
 export default App;
--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,101 +1,82 @@
-import React from 'react';
-import axios from 'axios';
-import Chart from 'chart.js';
-
-
-var dataSource = {
-    datasets: [
-        {
-            data: [],
-            backgroundColor: [
-                '#ffcd56',
-                '#ff6384',
-                '#36a2eb',
-                '#fd6b19',
-                'green',
-                'yellow',
-                'purple',
-
-            ],
-        }
-    ],
-    labels: []
-};
-function createChart() {
-    var ctx = document.getElementById("myChart").getContext("2d");
-    Chart.defaults.global.maintainAspectRatio = false;
-    var myPieChart = new Chart(ctx, {
-        type: 'pie',
-        data: dataSource
-    });
-}
-
-function getBudget() {
-    axios.get('http://localhost:3001/budget')
-    .then(function (res) {
-        console.log(res);
-        for (var i = 0; i < res.data.myBudget.length; i++) {
-            dataSource.datasets[0].data[i] = res.data.myBudget[i].budget;
-            dataSource.labels[i] = res.data.myBudget[i].title;
-        }
-        createChart();
-    });
-    
-}
-
+import React, { useState, useEffect } from "react";
+import { Pie } from "react-chartjs-2";
+import axios from "axios";
 
 function HomePage() {
-    getBudget();
+  const [chartData, setChart] = useState({});
+  useEffect(() => {
+    axios.get("http://localhost:3001/budget").then(function (res) {
+      let data = [];
+      let labels = [];
+      for (let i = 0; i < res.data.myBudget.length; i++) {
+        data.push(res.data.myBudget[i].budget);
+        labels.push(res.data.myBudget[i].title);
+      }
+
+      setChart({
+        labels: labels,
+        datasets: [
+          {
+            data: data,
+            backgroundColor: [
+              "black",
+              "blue",
+              "orange",
+              "red",
+              "green",
+              "yellow",
+              "purple",
+            ],
+          },
+        ],
+      });
+    });
+  });
+
   return (
     <main className="container center" id="main">
-
-    <div className="page-area">
-
-        
+      <div className="page-area">
         <article className="text-box">
-            <h1>Stay on track</h1>
-            <p>
-                Do you know where you are spending your money? If you really stop to track it down,
-                you would get surprised! Proper budget management depends on real data... and this
-                app will help you with that!
-            </p>
-        </article>
-        
-
-        <article className="text-box">
-            <h1>Alerts</h1>
-            <p>
-                What if your clothing budget ended? You will get an alert. The goal is to never go over the budget.
-            </p>
+          <h1>Stay on track</h1>
+          <p>
+            Do you know where you are spending your money? If you really stop to
+            track it down, you would get surprised! Proper budget management
+            depends on real data... and this app will help you with that!
+          </p>
         </article>
 
         <article className="text-box">
-            <h1>Results</h1>
-            <p>
-                People who stick to a financial plan, budgeting every expense, get out of debt faster!
-                Also, they to live happier lives... since they expend without guilt or fear... 
-                because they know it is all good and accounted for.
-            </p>
+          <h1>Alerts</h1>
+          <p>
+            What if your clothing budget ended? You will get an alert. The goal
+            is to never go over the budget.
+          </p>
         </article>
 
         <article className="text-box">
-            <h1>Chart</h1>
-            <p>
-                <canvas id ="myChart" width="400" height="400"></canvas>
-            </p>
+          <h1>Results</h1>
+          <p>
+            People who stick to a financial plan, budgeting every expense, get
+            out of debt faster! Also, they to live happier lives... since they
+            expend without guilt or fear... because they know it is all good and
+            accounted for.
+          </p>
         </article>
-    </div>
-    
-  
-  
-    
-</main>
 
-
+        <article className="text-box">
+          <h1>Chart</h1>
+          <div>
+            <Pie
+              data={chartData}
+              width={400}
+              height={400}
+              options={{ maintainAspectRatio: false }}
+            />
+          </div>
+        </article>
+      </div>
+    </main>
   );
-
 }
-
-
 
 export default HomePage;
